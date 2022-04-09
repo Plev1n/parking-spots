16b152a1ab7c7e9bb69923bd6e89861c49ed81c0
David Pleva <plevadav@gmail.com>
2022-04-09 16:28:41 +0200

    feat: input components

---
+++ web-app/src/Components/Inputs/DatePicker/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import csLocale from "date-fns/locale/cs";
+
+const DatePickerCustom = () => {
+  const [date, setDate] = React.useState(new Date());
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns} locale={csLocale}>
+      <DateTimePicker
+        renderInput={(props) => <TextField {...props} />}
+        label="Kdy"
+        value={date}
+        onChange={(newValue) => {
+          setDate(newValue);
+        }}
+      />
+    </LocalizationProvider>
+  )
+}
+
+export default DatePickerCustom
---
+++ web-app/src/Components/Title/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div><p style={{height: "50px", fontSize: "30px"}}>Parkuj výhodně <span style={{fontSize: "10px"}}>nejen v Brně</span></p></div>
+  )
+}
+
+export default Title
