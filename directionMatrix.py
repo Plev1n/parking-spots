0c413995cf407049e0f12c4b2d100a89a281dde3
Petra Bartonova <artep.Bart@seznam.cz>
2022-04-09 20:43:30 +0200

    distance count and moduls repair

--- directionMatrix.py
+++ directionMatrix.py
@@ -1,17 +1,18 @@
 import googlemaps
-import pandas as pd
-import geopandas
 
 API_key = 'AIzaSyDy_tsEgUnqT0Pca81QJqzYVf_39Ox9IH4'#enter Google Maps API key
 gmaps = googlemaps.Client(key=API_key)
 
-def countTimeDistance (originLat, originLon, destinationLat, destinationLon ,method):
-    list = [0]
+def countTimeDistance (originLat, originLon, destinationLat, destinationLon,method):
+    list = [0,0]
+    jsonv = [0]
 
     origins = (originLat,originLon)
 
     destination = (destinationLat,destinationLon)
 
-    list = gmaps.distance_matrix(origins, destination, mode=method)
+    jsonv = gmaps.distance_matrix(origins, destination, mode=method)
+    list[0] = jsonv.get('rows')[0].get('elements')[0].get('distance').get('value')  #distance
+    list[1] = jsonv.get('rows')[0].get('elements')[0].get('duration').get('value')  #time
       
     return list
