1d729f9b108de9b12809b2f70a14477d9960aaec
David Pleva <plevadav@gmail.com>
2022-04-10 00:08:31 +0200

    feat/results

--- web-app/src/Pages/HomeScreen/index.jsx
+++ web-app/src/Pages/HomeScreen/index.jsx
@@ -3,17 +3,19 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import TimeSlider from '../../Components/Inputs/TimeSlider';
 import Location from '../../Components/Inputs/Location';
-import WhenInput from '../../Components/Inputs/WhenInput';
+import DatePickerCustom from '../../Components/Inputs/DatePicker';
 import Title from '../../Components/Title';
 import Preferences from '../../Components/Inputs/Preferences';
 import axios from 'axios';
 
 export const HomeScreen = props => {
-  const [time, setTime] = React.useState('');
+  const [time, setTime] = React.useState(new Date());
   const [location, setLocation] = React.useState('');
+  const [parkingTime, setParkingTime] = React.useState(3600);
 
   const handleSubmit = () => {
-    if (time === '' || location === '') return
+    if (location === '') return
+    console.log(time, location, parkingTime)
     axios.post('http://127.0.0.1:5000/get_parking_spot', {
       time, 
       startLocation: location
@@ -31,10 +33,10 @@ export const HomeScreen = props => {
       <Stack spacing={2}>
         <Title />
         <Location setLocation={setLocation} />
-        <WhenInput setTime={setTime}/>
-        <TimeSlider />
-        <Preferences />
-        <Button onClick={handleSubmit} variant="outlined">Hledat</Button>
+        <DatePickerCustom setTime={setTime} time={time}/>
+        <TimeSlider setParkingTime={setParkingTime} parkingTime={parkingTime}/>
+        {/* <Preferences /> */}
+        <Button onClick={handleSubmit} variant="contained">Hledat</Button>
       </Stack>
     </div>
   )
