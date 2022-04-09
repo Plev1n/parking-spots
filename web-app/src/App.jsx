2682004a2f5de760daa15d47744c06dc17f686d7
David Pleva <plevadav@gmail.com>
2022-04-09 10:17:38 +0200

    feat: materialUI init

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Button from '@mui/material/Button';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,9 +12,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <Button variant='contained' onClick={() => setCount((count) => count + 1)}>
             count is: {count}
-          </button>
+          </Button>
         </p>
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
