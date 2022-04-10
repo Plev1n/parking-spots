9e308b176a8bbb5e6e8041ad03a1f00cca9dd7cc
Petra Skácelová <pskacelova16@gmail.com>
2022-04-10 08:37:07 +0200

    cleanup

--- api-server/app.py
+++ api-server/app.py
@@ -1,5 +1,4 @@
 from flask import Flask,request
-# from re import I
 import requests
 import json
 import geojson
@@ -8,12 +7,12 @@ import pandas as pd
 import geopandas
 import math
 from datetime import datetime
-# from pandas.io.json import json_normalize
 from flask_cors import CORS
-from models import ParkingSpot, ParkingHouse, UserRequest
+from models import ParkingHouse, UserRequest
 
 app = Flask(__name__)
 CORS(app)
+
 @app.route("/pay", methods=['POST'])
 def pay():
      return "test"
@@ -26,10 +25,7 @@ def get_parking_spot():
     return obj_json(df_street), to_json_houses(df_house)
 
 def get_coordinates(): 
-
     urls_Geo = "https://opendata.arcgis.com/datasets/9a2eda1b0c604886b0dd1eb548fb351b_0.geojson"
-
-
     response_API_geojsons = requests.get(urls_Geo)
     data_geojson = response_API_geojsons.text
     parse_geojson = geojson.loads(data_geojson)
@@ -39,26 +35,16 @@ def get_coordinates():
     parking_raw = parkingPaid
     df_zones = zones(parking_raw).reset_index()
 
-    #parking_geo = parking_raw.merge(df_zones, on="index", how="right")
     #vyházení koordinátů z polygonů, výstup json_coordinates[id,x,y]
     #verze pro API
     df_coordinates = pd.DataFrame({"index": [], "Longitude": [], "Latitude": []}).set_index("index")
     for num, place in parking_raw.iterrows():
         coordinates = place[0][0].exterior.coords[:-1][1]
         df_coordinates.loc[num] = [coordinates[0], coordinates[1]]
-
-    #print(df_coordinates.reset_index().columns)
-    #print(df_zones.reset_index().columns)
     df_coordinates = df_coordinates.reset_index()
     df_coordinates = df_coordinates.merge(df_zones, on="index", how="right")
 
-    #df_merge = coordinates.merge(zones, on="id").set_index("id")
-
     #parkoviště
-    #parking_house = get_parking_house()[["attributes.Latitude", "attributes.Longitude"]]
-    #parking_house = parking_house.rename(columns={"attributes.Latitude": "Latitude", "attributes.Longitude": "Longitude"})
-    #df_merge = pd.concat([coordinates, parking_house], ignore_index=True)
-
     return df_coordinates
 
 #ZAPOJIT PARKOVACÍ DOMY
@@ -71,9 +57,7 @@ def get_parking_house():
     parkingHouse = pd.json_normalize(parkingHouse)
 
     #parkoviště
-    parking_house = parkingHouse[["attributes.name", "attributes.Latitude", "attributes.Longitude", "attributes.CapacityForPublic", "attributes.spacesAllUsersVacant"]]
-
-    return parking_house
+    return parkingHouse[["attributes.name", "attributes.Latitude", "attributes.Longitude", "attributes.CapacityForPublic", "attributes.spacesAllUsersVacant"]]
 
 def parking_houses_calc(df, coordinates):
     df = df.reset_index()
@@ -97,7 +81,6 @@ def houses_main(dct):
     df = get_parking_house()
     df_filtred = parking_houses_calc(df,[dct["finishLat"], dct["finishLon"]])
     df_choice = count_walking(df_filtred, dct["finishLat"], dct["finishLon"])
-    #print(df_choice)
     df_choice = count_driving(df_choice, dct["startLat"], dct["startLon"])
     return df_choice
 
@@ -119,7 +102,6 @@ def zones(parking_places_raw):
     parse_geojson = geojson.loads(data_geojson)
     parkingZones = geopandas.GeoDataFrame.from_features(parse_geojson)
     zony_raw = parkingZones
-
     zona_a = zony_raw[zony_raw["typzony_t"] == "zóna A"]
     zona_b = zony_raw[zony_raw["typzony_t"] == "zóna B"]
 
@@ -134,17 +116,10 @@ def zones(parking_places_raw):
         else: 
             for zona in zona_a.iterrows():
                 if zona[1][0].contains(place[0]): 
-                    #parking_zones.loc[num] = "A"
                     break
             else: 
                 parking_zones.loc[num] = "C"
-
-    zones = parking_zones.reset_index().drop(columns=["id"])
-    #df = df.rename(columns={"index": "id"})
-
-    #merge_zones = df.merge(zones, on="id", how="inner")
-
-    return zones
+    return parking_zones.reset_index().drop(columns=["id"])
 
 def nearest(df, location, val_x, val_y):
     """vrátí seznam id parkovacích míst do 1 km"""
@@ -171,11 +146,9 @@ def count_walking(df, fin_lat, fin_lon):
         lst = countTimeDistance(df["Latitude"][id], df["Longitude"][id], fin_lat, fin_lon,method="walking")
         df["walking_distance_m"][id] = lst[0]
         df["walking_time_s"].loc[id] = lst[1]
-
         counter += 1
         if counter == 3:
             break
-
     return df
 
 def count_driving(df, start_lat, start_lon): 
@@ -188,41 +161,31 @@ def count_driving(df, start_lat, start_lon):
         lst = countTimeDistance(start_lat, start_lon, df["Latitude"][id], df["Longitude"][id], method="driving")
         df["driving_distance_m"].loc[id] = lst[0]
         df["driving_time_s"].loc[id] = lst[1]
-
         counter += 1
         if counter == 3:
             break
-
     return df
 
 def count_price(df,dct):
     parking_time = dct["parkingTime"]
     str_time = dct["timeISO"]
-
     hr = datetime.strptime(str_time, '%Y-%m-%dT%H:%M:%S.%f%z').hour
-
     df["price"] = 1
     if hr < 6 or hr > 17:
-        df["price"] = (math.ceil(parking_time/3600))*20
-    
+        df["price"] = (math.ceil(parking_time/3600))*20    
     lst = df[df["zona"] == "B"]["index"]
     for i in lst:
-        df["price"][i] == (math.ceil(parking_time/3600))*30
-    
+        df["price"][i] == (math.ceil(parking_time/3600))*30    
     return df.drop(columns=["zona"])
 
-
 def main(dct): 
-    #print(dct)
     df = get_coordinates()
     lst_nearest = nearest(df, [dct["finishLat"], dct["finishLon"]], 0.005, 0.005)
-    #print(len(lst_nearest))
     df_choice = filter_nearest(lst_nearest, df)
     df_choice = count_walking(df_choice, dct["finishLat"], dct["finishLon"])
     df_choice = count_driving(df_choice, dct["startLat"], dct["startLon"])
     df_choice = count_price(df_choice, dct)
     
-    #print(df_choice[df_choice["walking_distance_m"] > 0])
     return df_choice[df_choice["walking_distance_m"] > 0].sort_values(by=["walking_distance_m"])
 
 def countTimeDistance (originLat, originLon, destinationLat, destinationLon,method):
@@ -230,23 +193,12 @@ def countTimeDistance (originLat, originLon, destinationLat, destinationLon,meth
     gmaps = googlemaps.Client(key=API_key)
     list = [0,0]
     jsonv = [0]
-
     origins = (originLat,originLon)
-
     destination = (destinationLat,destinationLon)
-
     jsonv = gmaps.distance_matrix(origins, destination, mode=method)
     list[0] = jsonv.get('rows')[0].get('elements')[0].get('distance').get('value')  #distance
-    list[1] = jsonv.get('rows')[0].get('elements')[0].get('duration').get('value')  #time
-      
+    list[1] = jsonv.get('rows')[0].get('elements')[0].get('duration').get('value')  #time     
     return list
-
-#location = (49.1985325, 16.6074342)
-#koordinaty, df = get_coordinates()
-#print(zones(koordinaty, df))
-#abc = UserRequest()
-#print(df.columns)
-#print(df)
 
 if __name__ == '__main__':
     app.run()
